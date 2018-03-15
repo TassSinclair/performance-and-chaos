8960c607692c88e5e936c2d2662370dccd6236c3
Andrew Hamilton <andrew.hamilton@thoughtworks.com>
2018-03-16 00:25:12 +1100

    Promoting 'minikube ip' hostname eval to a variable

--- load-testing.mk
+++ load-testing.mk
@@ -1,5 +1,6 @@
 GATLING_IMAGE=denvazh/gatling:2.3.1
 LOCUST_IMAGE=sinclairstudios/locust:chaosworkshop
+HOST=http://$(shell minikube ip):30001
 get-gatling:
 	docker pull ${GATLING_IMAGE}
 run-gatling:
@@ -7,7 +8,7 @@ run-gatling:
 		-v $(CURDIR)/gatling/conf:/opt/gatling/conf \
 		-v $(CURDIR)/gatling/user-files:/opt/gatling/user-files \
 		-v $(CURDIR)/gatling/results:/opt/gatling/results \
-		-e JAVA_OPTS="-Dusers=500 -Dduration=30 -Dhost=http://$(shell minikube ip):30001" \
+		-e JAVA_OPTS="-Dusers=500 -Dduration=30 -Dhost=${HOST}" \
 		${GATLING_IMAGE} \
 		-s GatlingSimulation
 show-gatling-reports:
@@ -19,6 +20,5 @@ run-locust:
 	docker run -it --rm \
 	-v $(CURDIR)/locust:/config \
 	${LOCUST_IMAGE} \
-	-h http://$(shell minikube ip):30001 \
-	-c 200 \
-	-r 500
+	-h ${HOST} \
+	-c 200 -r 500
