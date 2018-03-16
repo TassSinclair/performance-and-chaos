497e2244f1593030e4626589d7fc6716b5cd6639
Andrew Hamilton <andrew.hamilton@thoughtworks.com>
2018-03-16 15:15:27 +1100

    Fixing the locustssssss

--- load-testing.mk
+++ load-testing.mk
@@ -1,6 +1,5 @@
 GATLING_IMAGE=denvazh/gatling:2.3.1
 LOCUST_IMAGE=sinclairstudios/locust:chaosworkshop
-HOST=http://$(shell minikube ip):30001
 get-gatling:
 	docker pull ${GATLING_IMAGE}
 run-gatling:
@@ -8,7 +7,7 @@ run-gatling:
 		-v $(CURDIR)/gatling/conf:/opt/gatling/conf \
 		-v $(CURDIR)/gatling/user-files:/opt/gatling/user-files \
 		-v $(CURDIR)/gatling/results:/opt/gatling/results \
-		-e JAVA_OPTS="-Dusers=500 -Dduration=30 -Dhost=${HOST}" \
+		-e JAVA_OPTS="-Dusers=500 -Dduration=30 -Dhost=http://$(shell minikube ip):30001" \
 		${GATLING_IMAGE} \
 		-s GatlingSimulation
 show-gatling-reports:
@@ -20,5 +19,5 @@ run-locust:
 	docker run -it --rm \
 	-v $(CURDIR)/locust:/config \
 	${LOCUST_IMAGE} \
-	-h ${HOST} \
+	-h $(shell minikube ip):30001 \
 	-c 200 -r 500
